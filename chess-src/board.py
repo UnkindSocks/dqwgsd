cb34b2102e088f064735c6b76cc4ea33268994b8
brian <leonardolyle@outlook.com>
2023-03-04 18:54:55 +0000

    moving-chesss

--- chess-src/board.py
+++ chess-src/board.py
@@ -1,6 +1,7 @@
 from const import *
 from square import Square
 from piece import *
+from move import Move
 
 class Board:
 
@@ -10,6 +11,54 @@ class Board:
         self._create()
         self._add_pieces('white')
         self._add_pieces('black')
+
+
+    def calc_moves(self, piece, row, col):
+        # calculate all the possible or valid moves of a specific piece in a specific position
+
+        def pawn_moves():
+                # steps
+                steps = 1 if piece.moved else 2
+
+                # vertical moves
+                start = row + piece.dir
+                end = row + (piece.dir * (1 + steps))
+
+                # diagonal moves
+
+            
+        def knight_moves():
+            # 8 possible moves
+            possible_moves = [
+                (row-2, col+1),
+                (row-1, col+2),
+                (row+1, col+2),
+                (row+2, col+1),
+                (row+2, col-1),
+                (row+1, col-2),
+                (row-1, col-2),
+                (row-2, col-1),
+            ]
+        
+            for possible_move in possible_moves:
+                possible_move_row, possible_move_col = possible_move
+
+                if Square.in_range(possible_move_row, possible_move_col):
+                    if self.squares[possible_move_row][possible_move_col].isempty_or_rival(piece.color):
+                        # create squares of the new move
+                        initial = Square(row, col)
+                        final = Square(possible_move_row, possible_move_col) # piece=piece
+                        # create new move
+                        move = Move(initial, final)
+                        # append new valid move
+                        piece.add_move(move)
+
+        if isinstance(piece, Pawn): pawn_moves()
+        elif isinstance(piece, Knight): knight_moves()
+        elif isinstance(piece, Bishop): pass
+        elif isinstance(piece, Rook): pass
+        elif isinstance(piece, Queen): pass
+        elif isinstance(piece, King): pass
 
     def _create(self):
         for row in range(ROWS):
